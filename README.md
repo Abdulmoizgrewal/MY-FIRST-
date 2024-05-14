2f0bc60c10042da05e0fa3f10d94479ae151d464
abdulmoizgrewal <abdulmoiz182838@gmail.com>
2024-05-14 16:44:43 +0500

    i m a begineer

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# MY-FIRST-
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world";
+}
---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,i;
+   cout<<"enter any num"<<endl;
+   cin>>a;
+  for(i=0; i<=10; i++)
+  { cout<<"the table of number is<<a*i<<endl;  }
+   
+    return 0;
+}
